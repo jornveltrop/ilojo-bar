b5ca263b11f66d6a68a261950285b708860f6d2d
Jeany de Vries <jeanydevries@hotmail.com>
2022-05-31 11:57:08 +0200

    made logic home page in seperate script

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,5 @@
-let buttonoHomePage;
-
 function init() {
-    buttonoHomePage = document.getElementById("buttonHomePage");
-    buttonoHomePage.addEventListener("click", goToNav);
+
 }
 
 init();
-
-function goToNav(){
-    window.location.href += "discover";
-}
