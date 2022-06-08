849f0eb651582d9c7a8c8239321a9275c2b82106
jornveltrop <jorn1veltrop@gmail.com>
2022-06-08 15:01:56 +0200

    Gevels test

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,11 @@
-function init() {
+gsap.registerPlugin(ScrollTrigger)
 
-}
-
-init();
+ScrollTrigger.create({
+	trigger: '.img_cont',
+	animation: gsap.fromTo('.scroll_cont', {scale: 1 }, {scale: 2}),
+	pin: true,
+	start: 'center center',
+	end: 'bottom left',
+	scrub: 1, // I like the 1 sec delay, set to true for exact anime on scroll
+	//markers: true,
+})
