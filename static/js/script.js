2711f7e3586b02b447096d28746bb010a675548f
jornveltrop <jorn1veltrop@gmail.com>
2022-06-09 12:02:05 +0200

    Greensock

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,7 @@ gsap.registerPlugin(ScrollTrigger)
 let tl = gsap.timeline();
 
 tl.from("header img", {translateY: "-210%", rotation: "-10", duration: 2.5, ease: "elastic", delay: .5})
+  .to(".create", { y: "0%", duration: 0.7, stagger: 0.2 }, "<25%");
 
 
 
@@ -21,3 +22,5 @@ scrollTL.to('.scroll_cont', {scale: 1.6} )
         .fromTo('#color_building', {opacity: 0}, {opacity: 1},"<")
         .to('#background', {opacity: 0},"-=50%")
         .to('#overlay', {opacity: 0, scale: 1.05}, "<25%")
+
+
