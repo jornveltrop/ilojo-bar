2a55e37a6813529e0f5e008d74f685cecf1b62bf
jornveltrop <jorn1veltrop@gmail.com>
2022-06-10 16:53:52 +0200

    Home and windows fix

--- static/js/script.js
+++ static/js/script.js
@@ -1,37 +1,42 @@
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
-gsap.defaults({overwrite: "auto"});
+const homepage = document.querySelector('.home')
 
-let tl = gsap.timeline();
+if (homepage) {
+  gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
+  gsap.defaults({overwrite: "auto"});
+  
+  let tl = gsap.timeline();
 
-tl.from("header img", {translateY: "-210%", rotation: "-10", duration: 2.5, ease: "elastic", delay: .5})
-  .to(".create", { y: "0%", duration: 0.7, stagger: 0.2 }, "<15%")
-  .from(".scroll_btn", {translateY: "300px", ease: Power1.easeOut}, "<")
-
-
-
-let scrollTL = gsap.timeline({
-    scrollTrigger: {
-      trigger: '.img_cont',
-      start: 'top top',
-      end: 'bottom top',
-      scrub: 1,
-      pin: true,
-      snap: 4
-    }
-});
-
-scrollTL.to('.scroll_btn', {opacity: 0, duration: .3} )
-        .to('.scroll_cont', {scale: 1.6},"<")
-        .fromTo('#color_building', {opacity: 0}, {opacity: 1},"<")
-        .to('#background', {opacity: 0},"-=50%")
-        .to('#overlay', {opacity: 0, scale: 1.05}, "<25%")
-        .fromTo('.discover_cont',{opacity: 0, zIndex: 0, scale: 0.9, rotation:-3}, {opacity: 1, zIndex: 2, scale: 1, rotation:0}, "<")
-        .addLabel('end');
+  tl.from("header img", {translateY: "-210%", rotation: "-10", duration: 2.5, ease: "elastic", delay: .5})
+    .to(".mask_span", { y: "0%", duration: 0.7, stagger: 0.2 }, "<15%")
+    .from(".scroll_btn", {translateY: "300px", ease: Power1.easeOut}, "<")
 
 
-let btn = document.querySelector('footer button')
 
-btn.addEventListener("click", () => {
-  let scrollValue = scrollTL.scrollTrigger.labelToScroll('end')
-  gsap.to(window, {scrollTo: scrollValue, duration: 2.2});
-});
+  let scrollTL = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.img_cont',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: 1,
+        pin: true,
+        snap: 4
+      }
+  });
+
+  scrollTL.to('.scroll_btn', {opacity: 0, duration: .3} )
+          .to('.scroll_cont', {scale: 1.6},"<")
+          .fromTo('#color_building', {opacity: 0}, {opacity: 1},"<")
+          .to('#background', {opacity: 0},"-=50%")
+          .to('#overlay', {opacity: 0, scale: 1.05}, "<25%")
+          .fromTo('.discover_cont',{opacity: 0, zIndex: 0, scale: 0.9, rotation:-3}, {opacity: 1, zIndex: 2, scale: 1, rotation:0}, "<")
+          .addLabel('end');
+
+
+  let btn = document.querySelector('footer button')
+
+  btn.addEventListener("click", () => {
+    let scrollValue = scrollTL.scrollTrigger.labelToScroll('end')
+    gsap.to(window, {scrollTo: scrollValue, duration: 2.2});
+  });
+
+}
