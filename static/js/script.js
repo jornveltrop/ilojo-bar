820b9b7edd34a6305b87ae5ef31c16d86832a3cc
Jeany de Vries <jeanydevries@hotmail.com>
2022-06-10 14:19:46 +0200

    back btn is now in header with history.back

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,12 @@
 function init() {
-
+    const goBackLink = document.querySelector(".goBack")
+    if(goBackLink){
+        const historyGoBack = () => {
+            window.history.back()
+            goBackLink.removeEventListener("click", historyGoBack)
+        }
+        goBackLink.addEventListener("click", historyGoBack)
+    }
 }
 
 init();
