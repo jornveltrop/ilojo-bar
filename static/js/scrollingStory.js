5713e1150946fa0ebf5d1e3d7176ec47a68fc4b7
Jeany de Vries <jeanydevries@hotmail.com>
2022-06-13 16:21:06 +0200

    added stone that rotates

--- static/js/scrollingStory.js
+++ static/js/scrollingStory.js
@@ -11,3 +11,15 @@ gsap.to(".wrecker", {
     margin: '100px 0 0 100%',
     transform: 'rotate(-30deg)'
   });
+
+  gsap.to(".rockImg", {
+    scrollTrigger: {
+      trigger: ".wrecker",
+      scrub: 4,
+      start : '-400 top',
+      end: 'bottom center'
+    }, 
+    ease: "linear",
+    margin: '50px 0 0 0px',
+    transform: 'rotate(-30deg)'
+  });
