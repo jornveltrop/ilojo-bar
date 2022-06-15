0d01fcf2b1f5166a95982557da204fc3caf10601
jornveltrop <jorn1veltrop@gmail.com>
2022-06-15 14:20:14 +0200

    details

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,7 @@
 const homepage = document.querySelector('.home')
+const discover = document.querySelector('.discover')
 
+//HOME PAGE CODE
 if (homepage) {
   gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
   gsap.defaults({overwrite: "auto"});
@@ -10,7 +12,6 @@ if (homepage) {
   tl.from("header img", {translateY: "-210%", rotation: "-10", duration: 2.5, ease: "elastic", delay: .5})
     .to(".mask_span", { y: "0%", duration: 0.7, stagger: 0.2 }, "<15%")
     .from(".scroll_btn", {translateY: "300px", ease: Power1.easeOut}, "<")
-    .from(".see_model", {translateY: "300px", ease: Power1.easeOut}, "<")
 
 
   let scrollTL = gsap.timeline({
@@ -30,8 +31,9 @@ if (homepage) {
           .to('#background', {opacity: 0},"-=50%")
           .to('#overlay', {opacity: 0, scale: 1.05}, "<25%")
           .fromTo('.discover_cont',{opacity: 0, zIndex: 0, scale: 0.9, rotation:-3}, {opacity: 1, zIndex: 2, scale: 1, rotation:0}, "<")
-          .fromTo('.see_model',{opacity: 0, zIndex: 0, scale: 0.9, rotation:-3}, {opacity: 1, zIndex: 2, scale: 1, rotation:0}, "<")
-          .addLabel('end');
+          .from('.see_model_cont',{ translateX: "100%", opacity: "0"}, "<")
+          .addLabel('end')
+         
 
 
   let btn = document.querySelector('footer button')
@@ -42,7 +44,42 @@ if (homepage) {
   });
 }
 
-init();
+
+// DISCOVER PAGE CODE
+if (discover) {
+  gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
+  gsap.defaults({overwrite: "auto"});
+  
+
+  let scrollTL = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.img_cont',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: 1,
+        pin: true,
+        snap: 4
+      }
+  });
+
+  scrollTL.to('.scroll_btn', {opacity: 0, duration: .3} )
+          .to('.scroll_cont', {scale: 1.6},"<")
+          .fromTo('#color_building', {opacity: 0}, {opacity: 1},"<")
+          .to('#background', {opacity: 0},"-=50%")
+          .to('#overlay', {opacity: 0, scale: 1.05}, "<25%")
+          .fromTo('.discover_cont',{opacity: 0, zIndex: 0, scale: 0.9, rotation:-3}, {opacity: 1, zIndex: 2, scale: 1, rotation:0}, "<")
+          .from('.see_model_cont',{ translateX: "100%", opacity: "0"}, "<")
+          .addLabel('end')
+         
+
+
+  let btn = document.querySelector('footer button')
+
+  btn.addEventListener("click", () => {
+    let scrollValue = scrollTL.scrollTrigger.labelToScroll('end')
+    gsap.to(window, {scrollTo: scrollValue, duration: 2.2});
+  });
+}
 
 function init() {
   const goBackLink = document.querySelector(".goBack")
@@ -54,3 +91,5 @@ function init() {
       goBackLink.addEventListener("click", historyGoBack)
   }
 }
+
+init();
