420ada30522049e95e875721f438bb2d07c93428
Jeany de Vries <jeanydevries@hotmail.com>
2022-06-15 10:59:43 +0200

    small css changes

--- static/js/scrollingStory.js
+++ static/js/scrollingStory.js
@@ -4,8 +4,8 @@ gsap.to(".wrecker", {
     scrollTrigger: {
       trigger: ".wrecker",
       scrub: 4,
-      start : '-400 top',
-      end: 'bottom center'
+      start : '-500px top',
+      end: 'bottom center',
     }, 
     ease: "linear",
     margin: '100px 0 0 100%',
@@ -16,7 +16,7 @@ gsap.to(".wrecker", {
     scrollTrigger: {
       trigger: ".wrecker",
       scrub: 4,
-      start : '-400 top',
+      start : '-800px top',
       end: 'bottom center'
     }, 
     ease: "linear",
