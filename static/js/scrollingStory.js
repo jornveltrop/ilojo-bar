523c3799cd09a1985f9e72eaafdc58de3983bcaf
Jeany de Vries <jeanydevries@hotmail.com>
2022-06-22 13:36:45 +0200

    wrecking ball overflow is now hidden

--- static/js/scrollingStory.js
+++ static/js/scrollingStory.js
@@ -11,15 +11,3 @@ gsap.to(".wrecker", {
     margin: '100px 0 0 100%',
     transform: 'rotate(-30deg)'
   });
-
-  gsap.to(".rockImg", {
-    scrollTrigger: {
-      trigger: ".wrecker",
-      scrub: 4,
-      start : '-800px top',
-      end: 'bottom center'
-    }, 
-    ease: "linear",
-    margin: '50px 0 0 0px',
-    transform: 'rotate(-30deg)'
-  });
